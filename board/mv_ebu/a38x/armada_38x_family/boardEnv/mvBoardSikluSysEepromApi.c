3b520b2989f416ca4bf6bbb6a6823c94181c8817
edwardk <edwardk@siklu.com>
2016-07-12 16:57:46 +0000

    add SYSEEPROM support (yet not connected)
    git-svn-id: http://192.168.42.160/software/main@17639 ce24552a-085e-4656-8849-d4bfd34db3c6

---
+++ board/mv_ebu/a38x/armada_38x_family/boardEnv/mvBoardSikluSysEepromApi.c
@@ -0,0 +1,397 @@
+/*
+ * mvBoardSikluSysEepromApi.c
+ *
+ *  Created on: Jul 12, 2016
+ *      Author: edwardk
+ */
+
+#include <common.h>
+#include <linux/string.h>
+#include <linux/ctype.h>
+#include <crc.h>
+
+#include <spi_flash.h>
+#include "siklu_eeprom.h"
+
+// follow declarations should be before "siklu_eeprom_data.h" file!
+
+#define NVRAM_SERIAL_NUM_FIELD_MAX_SIZE 20
+#define NVRAM_PRODUCT_NAME_FIELD_SIZE   20 // field size limited to 30 bytes
+#define AUX_A2D_CALIB_FLOAT_STRING_LEN  15
+
+#include "siklu_eeprom_data.h"
+
+DECLARE_GLOBAL_DATA_PTR;
+
+// const int SERIAL_SYS_EEPROM_HEADER_OFFSET = 0;
+
+static seeprom_S siklu_mrv_system_seeprom;
+
+static int seeprom_write2nvram_new_data_v1(void) {
+	int rc = 0;
+
+	// after complete  init all TLV set header info
+	siklu_mrv_system_seeprom.seeprom_board_info.header.crc.val = crc32(
+			(unsigned int) SYS_EEPROM_CRC_SEED_PRIM_VAL,
+			(unsigned char *) &siklu_mrv_system_seeprom.seeprom_board_info.data,
+			(int) sizeof(seeprom_data_S));
+
+	// last step - write data to seeprom
+	seeprom_erase_all_section();  // SPI NOR FLASH should be erased before write
+	mdelay(100);
+	rc = spi_flash_write(_get_spi_flash_data(), get_syseeprom_area_snor_offs(),
+			sizeof(seeprom_S), &siklu_mrv_system_seeprom);
+	if (rc != 0) {
+		printf("%s(): ERROR write data\n", __func__);
+		return -1;
+	}
+
+	return rc;
+}
+
+/*
+ *
+ *
+ */
+static int seeprom_read_fields_from_eeprom_v1(void) //
+{
+	unsigned char *pseeprom = (unsigned char*) &siklu_mrv_system_seeprom;
+	int rc = 0;
+	__u32 crc;
+	static int is_already_read = 0;
+
+	if ((is_already_read)) // read only once
+		return 0;
+
+	memset(pseeprom, 0x00, sizeof(seeprom_S));
+
+	size_t len = sizeof(seeprom_S);
+	rc = spi_flash_read(_get_spi_flash_data(), get_syseeprom_area_snor_offs(),
+			len, pseeprom);
+
+	if (rc != 0) {
+		printf(" Read Serial EEPROM FAIL\n");
+		return -1;
+	}
+	// Step 2 - analyze header
+	if (siklu_mrv_system_seeprom.seeprom_board_info.header.control_key.val
+			!= SYS_EEPROM_CONTROL_KEY_VAL) {
+		printf(" Control Key is Wrong. Expected 0x%x, Read 0x%x\n",
+		SYS_EEPROM_CONTROL_KEY_VAL,
+				siklu_mrv_system_seeprom.seeprom_board_info.header.control_key.val);
+		return -1;
+	}
+
+	// calculate and compare CRC
+	crc =
+			crc32(SYS_EEPROM_CRC_SEED_PRIM_VAL,
+					(__u8 *) &siklu_mrv_system_seeprom.seeprom_board_info.data,
+					(int) siklu_mrv_system_seeprom.seeprom_board_info.header.data_size.val);
+	if (crc != siklu_mrv_system_seeprom.seeprom_board_info.header.crc.val) {
+		printf(
+				"%s() Wrong CRC. Exists 0x%x, Calculated 0x%x, seeprom_raw_data %p, calculated size %d\n",
+				__func__,
+				siklu_mrv_system_seeprom.seeprom_board_info.header.crc.val, crc,
+				&siklu_mrv_system_seeprom, (int) sizeof(seeprom_data_S));
+		return -1;
+	}
+
+	is_already_read = 1;
+	return rc;
+}
+
+/*
+ *
+ */
+int seeprom_get_mac_v1(__u8* mac_addr)
+{
+	int rc = seeprom_read_fields_from_eeprom_v1();
+	if (rc == 0)
+	{
+		memcpy(mac_addr, siklu_mrv_system_seeprom.seeprom_board_info.data.mac.mac, NVRAM_MAC_SIZE);
+	}
+	else
+	{
+		printf("SEPROM data is wrong, write default values\n");
+		seeprom_primary_format_v1();
+
+		seeprom_read_fields_from_eeprom_v1();
+		memcpy(mac_addr, siklu_mrv_system_seeprom.seeprom_board_info.data.mac.mac,
+		NVRAM_MAC_SIZE);
+	}
+	return 0;
+}
+
+/*
+ *
+ *
+ */
+int seeprom_primary_format_v1(void) {
+	// seeprom_S seeprom;
+	unsigned char *pseeprom = (unsigned char*) &siklu_mrv_system_seeprom;
+	char* tmp;
+	int rc = 0;
+
+	printf("Primary format System EEPROM Ver 1. Please wait... ");
+
+	memset(pseeprom, 0x00, sizeof(seeprom_S));
+
+	siklu_mrv_system_seeprom.seeprom_board_info.header.control_key.val =
+	SYS_EEPROM_CONTROL_KEY_VAL;
+	siklu_mrv_system_seeprom.seeprom_board_info.header.major.va1 =
+			SYS_EEPROM_MAJOR_VER;
+	siklu_mrv_system_seeprom.seeprom_board_info.header.minor.va1 =
+			SYS_EEPROM_MINOR_VER1;
+
+	// Set MAC
+	siklu_mrv_system_seeprom.seeprom_board_info.data.mac.t =
+			PROTECTED_FLASH_TYPE_MAC;
+	siklu_mrv_system_seeprom.seeprom_board_info.data.mac.l = NVRAM_MAC_SIZE;
+	siklu_mrv_system_seeprom.seeprom_board_info.data.mac.mac[0] = 0x00;
+	siklu_mrv_system_seeprom.seeprom_board_info.data.mac.mac[1] = 0x24;
+	siklu_mrv_system_seeprom.seeprom_board_info.data.mac.mac[2] = 0xa4;
+	siklu_mrv_system_seeprom.seeprom_board_info.data.mac.mac[3] = 0x00;
+	siklu_mrv_system_seeprom.seeprom_board_info.data.mac.mac[4] = 0xde;
+	siklu_mrv_system_seeprom.seeprom_board_info.data.mac.mac[5] = 0xad;
+
+	// Set base board serial
+	siklu_mrv_system_seeprom.seeprom_board_info.data.serial_bb.t =
+			PROTECTED_FLASH_TYPE_BB_SERIAL_NUMBER;
+	siklu_mrv_system_seeprom.seeprom_board_info.data.serial_bb.l =
+			NVRAM_SERIAL_NUM_FIELD_MAX_SIZE;
+	tmp = "233333332";
+	memcpy(siklu_mrv_system_seeprom.seeprom_board_info.data.serial_bb.serial,
+			tmp, strlen(tmp));
+
+	// Set System serial
+	siklu_mrv_system_seeprom.seeprom_board_info.data.serial_sys.t =
+			PROTECTED_FLASH_TYPE_SYSTEM_SERIAL_NUMBER;
+	siklu_mrv_system_seeprom.seeprom_board_info.data.serial_sys.l =
+			NVRAM_SERIAL_NUM_FIELD_MAX_SIZE;
+	tmp = "";
+	memcpy(siklu_mrv_system_seeprom.seeprom_board_info.data.serial_sys.serial,
+			tmp, strlen(tmp));
+
+	// Set Network port types
+	siklu_mrv_system_seeprom.seeprom_board_info.data.netw_port.t =
+			PROTECTED_FLASH_TYPE_NETWORK_PORT_TYPE;
+	siklu_mrv_system_seeprom.seeprom_board_info.data.netw_port.l =
+			NVRAM_NETW_PORT_TYPE_FIELD_SIZE;
+	strcpy(siklu_mrv_system_seeprom.seeprom_board_info.data.netw_port.port_type,
+			"ccf");
+
+	// Set Product Name
+	siklu_mrv_system_seeprom.seeprom_board_info.data.product_name.t =
+			PROTECTED_FLASH_TYPE_PRODUCT_NAME;
+	siklu_mrv_system_seeprom.seeprom_board_info.data.product_name.l =
+	NVRAM_PRODUCT_NAME_FIELD_SIZE;
+	strcpy(
+			siklu_mrv_system_seeprom.seeprom_board_info.data.product_name.product,
+			"EH700");
+
+	// after complete  init all TLV set header info
+	siklu_mrv_system_seeprom.seeprom_board_info.header.num_tlv.val = 5;
+	siklu_mrv_system_seeprom.seeprom_board_info.header.data_size.val =
+			sizeof(seeprom_data_S);
+
+	rc = seeprom_write2nvram_new_data_v1();
+
+	printf(" Ok\n");
+	return rc;
+}
+
+int seeprom_get_system_serial_v1(char* val) {
+	int rc = 0;
+	rc = seeprom_read_fields_from_eeprom_v1();
+	if (rc == 0) {
+		memcpy(val,
+				siklu_mrv_system_seeprom.seeprom_board_info.data.serial_sys.serial,
+				NVRAM_SERIAL_NUM_FIELD_MAX_SIZE);
+	} else {
+		printf("%s() SEPROM data is wrong, write default values\n", __func__);
+		seeprom_primary_format_v1();
+		seeprom_read_fields_from_eeprom_v1();
+		memcpy(val,
+				siklu_mrv_system_seeprom.seeprom_board_info.data.serial_sys.serial,
+				NVRAM_SERIAL_NUM_FIELD_MAX_SIZE);
+	}
+	return rc;
+}
+
+int seeprom_set_system_serial_v1(char* val) {
+	int rc = 0;
+	printf("%s() Should never be called!\n", __func__);
+	return rc;
+}
+
+int seeprom_get_baseband_serial_v1(char* val) {
+	int rc = 0;
+	rc = seeprom_read_fields_from_eeprom_v1();
+	if (rc == 0) {
+		memcpy(val,
+				siklu_mrv_system_seeprom.seeprom_board_info.data.serial_bb.serial,
+				NVRAM_SERIAL_NUM_FIELD_MAX_SIZE);
+	} else {
+		printf("%s() SEPROM data is wrong, write default values\n", __func__);
+		seeprom_primary_format_v1();
+		seeprom_read_fields_from_eeprom_v1();
+		memcpy(val,
+				siklu_mrv_system_seeprom.seeprom_board_info.data.serial_bb.serial,
+				NVRAM_SERIAL_NUM_FIELD_MAX_SIZE);
+	}
+	return rc;
+}
+
+/*
+ * input: board serial number string, up to NVRAM_SERIAL_NUM_FIELD_MAX_SIZE digits
+ */
+int seeprom_set_baseband_serial_v1(char* val) {
+	int rc = 0;
+
+	seeprom_read_fields_from_eeprom_v1();
+	if (strlen(val) > NVRAM_SERIAL_NUM_FIELD_MAX_SIZE) {
+		printf("Baseband serial number too large: %d\n",
+				NVRAM_SERIAL_NUM_FIELD_MAX_SIZE);
+		return 0;
+	}
+	// Set base board serial
+	siklu_mrv_system_seeprom.seeprom_board_info.data.serial_bb.t =
+			PROTECTED_FLASH_TYPE_BB_SERIAL_NUMBER;
+	siklu_mrv_system_seeprom.seeprom_board_info.data.serial_bb.l =
+			NVRAM_SERIAL_NUM_FIELD_MAX_SIZE;
+	memset(siklu_mrv_system_seeprom.seeprom_board_info.data.serial_bb.serial, 0,
+			NVRAM_SERIAL_NUM_FIELD_MAX_SIZE);
+	memcpy(siklu_mrv_system_seeprom.seeprom_board_info.data.serial_bb.serial,
+			val, strlen(val));
+
+	rc = seeprom_write2nvram_new_data_v1();
+
+	return rc;
+}
+
+/*
+ * input string in form AA:BB:CC:DD:EE:FF
+ */
+int seeprom_set_mac_v1(__u8* val)
+{
+	int rc = 0;
+	__u8* m = siklu_mrv_system_seeprom.seeprom_board_info.data.mac.mac;
+	rc = seeprom_read_fields_from_eeprom_v1();
+
+	if (!(isxdigit(val[0]) && isxdigit(val[1]) //
+			&& isxdigit(val[3]) && isxdigit(val[4])//
+			&& isxdigit(val[6]) && isxdigit(val[7])//
+			&& isxdigit(val[9]) && isxdigit(val[10])//
+			&& isxdigit(val[12]) && isxdigit(val[13])//
+			&& isxdigit(val[15]) && isxdigit(val[16])))
+	{
+		printf(" The string includes non-hex characters\n");
+		return 0;
+	}
+
+	memset(m, 0, NVRAM_MAC_SIZE);
+	convert_mac_address(m, (const char*)val);
+
+	printf(" New MAC Address %02X%02X%02X%02X%02X%02X\n", m[0], m[1], m[2], m[3], m[4], m[5]);
+
+	// Set MAC
+	siklu_mrv_system_seeprom.seeprom_board_info.data.mac.t = PROTECTED_FLASH_TYPE_MAC;
+	siklu_mrv_system_seeprom.seeprom_board_info.data.mac.l = NVRAM_MAC_SIZE;
+
+	rc = seeprom_write2nvram_new_data_v1();
+
+	return rc;
+}
+
+int seeprom_get_product_name_v1(char* product_name) {
+	int rc = 0;
+	rc = seeprom_read_fields_from_eeprom_v1();
+	if (rc == 0) {
+		memcpy(product_name,
+				siklu_mrv_system_seeprom.seeprom_board_info.data.product_name.product,
+				NVRAM_PRODUCT_NAME_FIELD_SIZE);
+	} else {
+		printf("%s() SEPROM data is wrong, write default values\n", __func__);
+		seeprom_primary_format_v1();
+		seeprom_read_fields_from_eeprom_v1();
+		memcpy(product_name,
+				siklu_mrv_system_seeprom.seeprom_board_info.data.product_name.product,
+				NVRAM_PRODUCT_NAME_FIELD_SIZE);
+	}
+	return rc;
+}
+
+int seeprom_set_product_name_v1(char* product_name) {
+	int rc = 0;
+
+	seeprom_read_fields_from_eeprom_v1();
+
+	if (strlen(product_name) > NVRAM_PRODUCT_NAME_FIELD_SIZE) {
+		printf("Product name too long: %d\n", NVRAM_PRODUCT_NAME_FIELD_SIZE);
+		return 0;
+	}
+	siklu_mrv_system_seeprom.seeprom_board_info.data.product_name.t =
+			PROTECTED_FLASH_TYPE_PRODUCT_NAME;
+	siklu_mrv_system_seeprom.seeprom_board_info.data.product_name.l =
+			NVRAM_PRODUCT_NAME_FIELD_SIZE;
+	memset(
+			siklu_mrv_system_seeprom.seeprom_board_info.data.product_name.product,
+			0, NVRAM_PRODUCT_NAME_FIELD_SIZE);
+	strcpy(
+			siklu_mrv_system_seeprom.seeprom_board_info.data.product_name.product,
+			product_name);
+
+	rc = seeprom_write2nvram_new_data_v1();
+	return rc;
+}
+
+/*
+ * input string exactly 4 characters:
+ *      'c' - copper
+ *      'f' - fiber
+ *      '-' - non-existant port
+ */
+int seeprom_set_netw_port_map_v1(const char* port_map) {
+	int rc = 0;
+	rc = seeprom_read_fields_from_eeprom_v1();
+
+	if (strlen(port_map) != 4) {
+		printf(
+				"Network port map should be exact 4 characters lens, received %d\n",
+				strlen(port_map));
+		return 0;
+	}
+	if (!(is_port_type_valid(port_map[0]) && is_port_type_valid(port_map[1])
+			&& is_port_type_valid(port_map[2])
+			&& is_port_type_valid(port_map[3]))) {
+		printf(
+				"Network port map values should be 'c'=copper or 'f'=fiber or '-'=no physical port\n");
+		return 0;
+	}
+
+	// update fields
+	// Set Network port types
+	siklu_mrv_system_seeprom.seeprom_board_info.data.netw_port.t =
+			PROTECTED_FLASH_TYPE_NETWORK_PORT_TYPE;
+	siklu_mrv_system_seeprom.seeprom_board_info.data.netw_port.l =
+			NVRAM_NETW_PORT_TYPE_FIELD_SIZE;
+	memcpy(siklu_mrv_system_seeprom.seeprom_board_info.data.netw_port.port_type,
+			port_map, NVRAM_NETW_PORT_TYPE_FIELD_SIZE);
+
+	rc = seeprom_write2nvram_new_data_v1();
+
+	return rc;
+}
+
+const char* seeprom_get_netw_port_map_v1(void) {
+	static char netw_port_map[NVRAM_NETW_PORT_TYPE_FIELD_SIZE + 1] = "----";
+	char* p =
+			siklu_mrv_system_seeprom.seeprom_board_info.data.netw_port.port_type;
+
+	seeprom_read_fields_from_eeprom_v1();
+
+	if ((is_port_type_valid(p[0]) && is_port_type_valid(p[1])
+			&& is_port_type_valid(p[2]) && is_port_type_valid(p[3])))
+		memcpy(netw_port_map, p, 4);
+	return netw_port_map;
+}
---
+++ board/mv_ebu/a38x/armada_38x_family/boardEnv/mvBoardSikluSysEepromCommon.c
@@ -0,0 +1,413 @@
+/*
+ * mvBoardSikluSysEepromCommon.c
+ *
+ *  Created on: Jul 12, 2016
+ *      Author: edwardk
+ */
+
+#include <common.h>
+#include <linux/string.h>
+#include <linux/ctype.h>
+#include <crc.h>
+#include <malloc.h>
+
+#include <spi_flash.h>
+#include "siklu_eeprom.h"
+
+extern struct spi_flash *get_spi_flash_data(void);
+
+DECLARE_GLOBAL_DATA_PTR;
+
+typedef struct {
+
+	int (*get_mac)(__u8* mac_addr);
+	int (*set_mac)(__u8* mac_addr);
+
+	int (*set_baseband_serial)(char* val);
+	int (*get_baseband_serial)(char* val);
+
+	int (*get_system_serial)(char* val);
+	int (*set_system_serial)(char* val);
+
+	int (*set_product_name)(char* product_name);
+	int (*get_product_name)(char* product_name);
+
+	const char* (*get_netw_port_map)(void);
+	int (*set_netw_port_map)(const char*);
+
+	int (*primary_format)(void);
+
+}seeprom_hndlr_S;
+// static seeprom_hndlr_S seeprom_hndlr;
+
+static seeprom_hndlr_S* GetHndlr(void) {
+	static seeprom_hndlr_S* seeprom_hndlr = NULL;
+	SYS_EEPROM_AREA_STATUS_E status;
+
+	if (seeprom_hndlr)
+		return seeprom_hndlr;
+
+	seeprom_hndlr = malloc(sizeof(seeprom_hndlr_S));
+	if (!seeprom_hndlr)
+		return NULL;
+
+	status = SYS_EEPROM_AREA_FORMAT_V1;
+	switch (status) {
+	case SYS_EEPROM_AREA_FORMAT_V1:
+
+		seeprom_hndlr->get_mac = seeprom_get_mac_v1;
+		seeprom_hndlr->set_mac = seeprom_set_mac_v1;
+
+		seeprom_hndlr->set_baseband_serial = seeprom_set_baseband_serial_v1;
+		seeprom_hndlr->get_baseband_serial = seeprom_get_baseband_serial_v1;
+
+		seeprom_hndlr->get_system_serial = seeprom_get_system_serial_v1;
+		seeprom_hndlr->set_system_serial = seeprom_set_system_serial_v1;
+
+		seeprom_hndlr->get_product_name = seeprom_get_product_name_v1;
+		seeprom_hndlr->set_product_name = seeprom_set_product_name_v1;
+
+		seeprom_hndlr->get_netw_port_map = seeprom_get_netw_port_map_v1;
+		seeprom_hndlr->set_netw_port_map = seeprom_set_netw_port_map_v1;
+		seeprom_hndlr->primary_format = seeprom_primary_format_v1;
+
+		break;
+	default:
+		printf("%s() Unknown SYSEEPROM AREA format %d\n", __func__, status);
+		free(seeprom_hndlr);
+		seeprom_hndlr = NULL;
+
+	}
+	return seeprom_hndlr;
+}
+
+__u32 get_syseeprom_area_snor_offs(void) {
+	// changed due to fact that all new sNOR have size 2M
+	// see also siklu_evb7000_sff.dt
+	const __u32 syseeprom_area_snor_offset = (2 * 1024 - 64) * 1024;
+	return syseeprom_area_snor_offset;
+}
+
+int is_port_type_valid(char port) {
+	switch (port) {
+	case 'c':
+	case 'f':
+	case '-':
+		return 1;
+		break;
+	default:
+		return 0;
+		break;
+	}
+	return 0;
+}
+
+/**
+ * set_mac_address - stores a MAC address into the EEPROM
+ *
+ * This function takes a pointer to MAC address string
+ * (i.e."XX:XX:XX:XX:XX:XX", where "XX" is a two-digit hex number) and
+ * stores it in one of the MAC address fields of the EEPROM local copy.
+ */
+void convert_mac_address(__u8* mac, const char *string)
+{
+	char *p = (char *) string;
+	unsigned int i;
+	if (!string)
+	{
+		printf("Usage: mac <n> XX:XX:XX:XX:XX:XX\n");
+		return;
+	}
+	for (i = 0; *p && (i < 6); i++)
+	{
+		mac[i] = simple_strtoul(p, &p, 16);
+		if (*p == ':')
+		p++;
+	}
+}
+
+/*
+ * quick and dirty access
+ */
+struct spi_flash *_get_spi_flash_data(void) {
+	struct spi_flash *flash = get_spi_flash_data();
+	if (flash == NULL) {       // need call probe
+		char *argv[] = { NULL };
+		extern int do_spi_flash_probe(int argc, char * const argv[]);
+		do_spi_flash_probe(1, argv);
+		flash = get_spi_flash_data();
+	}
+	return flash;
+}
+
+/*
+ *
+ * check and replace non printable characters
+ */
+void repair_string_printable(char* str) {
+	int i;
+	for (i = 0; str[i]; i++) {
+		if (str[i] != '-') // also '-' acceptable
+			if (!isalnum(str[i])) {
+				str[i] = '.';
+			}
+	}
+}
+
+static char repair_char_printable(char _char) {
+	char p = _char;
+
+	if (p != '-') // also '-' acceptable
+			{
+		if (!isalnum(p))
+			p = '.';
+	}
+	return p;
+}
+
+int seeprom_erase_all_section(void) {
+	int rc = 0;
+
+	// erase here whole SYSEEPROM sector in sNOR
+	rc = spi_flash_erase(_get_spi_flash_data(), get_syseeprom_area_snor_offs(),
+			_get_spi_flash_data()->sector_size);
+	return rc;
+}
+
+char* protect_string(char* str, int size) {
+	static char temp[100]; // take size enough large
+	memset(temp, 0, sizeof(temp));
+	if (size >= sizeof(temp))
+		size = sizeof(temp) - 1;
+	memcpy(temp, str, size);
+	return temp;
+}
+
+/*
+ * read header
+ * check control key
+ * read major and minor version
+ * decide status
+ */
+static int seeprom_check_data_validity(void) {
+	static int rc = -1;
+	seeprom_header_S header;
+	__u8 *pseeprom = (__u8 *) &header;
+
+	if ((rc >= 0)) // read only once
+		return rc;
+
+	memset(pseeprom, 0x00, sizeof(header));
+
+	size_t len = sizeof(header);
+	rc = spi_flash_read(_get_spi_flash_data(), get_syseeprom_area_snor_offs(),
+			len, pseeprom);
+	if (rc != 0) {
+		printf(" Read Serial EEPROM FAIL\n");
+		rc = -1;
+		return rc;
+	}
+
+	if (header.control_key.val != SYS_EEPROM_CONTROL_KEY_VAL) {
+		printf(" Control Key is Wrong. Expected 0x%x, Read 0x%x\n",
+		SYS_EEPROM_CONTROL_KEY_VAL, header.control_key.val);
+		rc = -1;
+		return rc;
+	}
+
+	if (header.major.va1 != SYS_EEPROM_MAJOR_VER) {
+		printf(" Wrong major version. Expected 0x%x, Read 0x%x\n",
+				header.major.va1, SYS_EEPROM_MAJOR_VER);
+		rc = -1;
+		return rc;
+	}
+
+	rc = 0;
+
+	return rc;
+}
+
+int siklu_get_mac_from_seeprom(__u8* mac_addr)
+{
+	seeprom_hndlr_S* hndlr = NULL;
+	int rc;
+
+	rc = seeprom_check_data_validity();
+	if (rc >= 0)
+	hndlr = GetHndlr();
+	if (hndlr)
+	return hndlr->get_mac(mac_addr);
+	else
+	return -1;
+}
+
+/*
+ * should be called after init sNOR and SYSEEPROM functionality!
+ *   returns number of ethernet ports according to syseeprom configuration string
+ */
+int siklu_get_seeprom_net_number_eth_ports(void) {
+	int i;
+	int rc = 0;
+	const char* p;
+
+	seeprom_hndlr_S* hndlr = NULL;
+
+	if (rc >= seeprom_check_data_validity())
+		hndlr = GetHndlr();
+
+	if (hndlr) {
+		p = hndlr->get_netw_port_map();     // seeprom_get_netw_port_map();
+		for (i = 0; i < NVRAM_NETW_PORT_TYPE_FIELD_SIZE; i++) {
+			if ((p[i] == 'c') || (p[i] == 'f')) {
+				rc++;
+			}
+		}
+	}
+	return rc;
+}
+/*
+ *  called on power up before first SYS EEPROM access
+ */
+void siklu_prepare_syseeprom(void) {
+	int rc = seeprom_check_data_validity();
+
+	if (rc >= 0) { // ok we have data
+		return;
+	}
+	// no SYSEEPROM data recognized, make default format
+	seeprom_hndlr_S* hndlr = GetHndlr(); // prepare area
+	if (hndlr) {
+		hndlr->primary_format();
+	}
+}
+
+/******************************************************************
+ *                      CLI
+ *
+ ******************************************************************/
+static int display_syseeprom_raw_data(void) {
+	int lines;
+	__u8 data[0x100];
+	__u8* p = data;
+	int offset = 0;
+
+	size_t len = sizeof(data);
+	spi_flash_read(_get_spi_flash_data(), get_syseeprom_area_snor_offs(), len,
+			data);
+	for (lines = 0; lines < 8; lines++) // read 8 * 16 bytes
+			{
+		offset = lines * 16;
+		//
+		printf(
+				"[%04x]: %02x.%02x.%02x.%02x.%02x.%02x.%02x.%02x.%02x.%02x.%02x.%02x.%02x.%02x.%02x.%02x  -  ",
+				offset, p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8],
+				p[9], p[10], p[11], p[12], p[13], p[14], p[15]);
+		printf(
+				"%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", //
+				repair_char_printable(p[0]), repair_char_printable(p[1]),
+				repair_char_printable(p[2]), //
+				repair_char_printable(p[3]), repair_char_printable(p[4]),
+				repair_char_printable(p[5]), //
+				repair_char_printable(p[6]), repair_char_printable(p[7]),
+				repair_char_printable(p[8]), //
+				repair_char_printable(p[9]), repair_char_printable(p[10]),
+				repair_char_printable(p[11]), //
+				repair_char_printable(p[12]), repair_char_printable(p[13]),
+				repair_char_printable(p[14]), //
+				repair_char_printable(p[15]));
+		p += 0x10;
+	}
+	return 0;
+}
+
+/*
+ *
+ *
+ *
+ */
+static int do_maintenance_sys_serial_eeprom(cmd_tbl_t *cmdtp, int flag,
+		int argc, char * const argv[]) //
+{
+	int rc = 0;
+
+	if (argc == 1) {
+		printf("Usage:\n%s\n", cmdtp->usage);
+		return 1;
+	}
+
+	if (strcmp(argv[1], "r") == 0) { // read raw data
+		display_syseeprom_raw_data();
+		return 0;
+	} //
+	if (strcmp(argv[1], "e") == 0) {
+		seeprom_erase_all_section();
+		return 0;
+	}
+
+	seeprom_hndlr_S* hndlr = NULL;
+
+	if (rc >= seeprom_check_data_validity())
+		hndlr = GetHndlr();
+
+	if (hndlr == NULL) {
+		printf("%s()  Error on line %d\n", __func__, __LINE__);
+		return 1;
+	}
+
+	if (strcmp(argv[1], "f") == 0) // parse SEEPROM data
+			{
+		__u8 mac[NVRAM_MAC_SIZE];
+		char data[20];
+		hndlr->get_mac(mac);
+		printf("MAC               %02x:%02x:%02x:%02x:%02x:%02x\n", mac[0],
+				mac[1], mac[2], mac[3], mac[4], mac[5]);
+
+		hndlr->get_baseband_serial(data);
+		printf("Board Serial      %s\n", data);
+
+		hndlr->get_system_serial(data);
+		printf("System Serial     %s\n", data);
+
+		hndlr->get_product_name(data);
+		printf("Product name      %s\n", data);
+
+		const char* p = hndlr->get_netw_port_map();
+		printf("Network port type %c%c%c%c\n", p[0], p[1], p[2], p[3]);
+	} //
+	else if (strcmp(argv[1], "w") == 0) // write default parameters
+			{
+		seeprom_erase_all_section(); // SPI NOR FLASH should be erased before write
+		mdelay(10);
+		hndlr->primary_format();
+		printf("\nReboot required before continue!\n");
+		//seeprom_primary_format();
+	} //
+
+	else if (strcmp(argv[1], "b") == 0) // set baseband serial
+			{
+		hndlr->set_baseband_serial(argv[2]);
+		// seeprom_set_baseband_serial(argv[2]);
+	} else if (strcmp(argv[1], "m") == 0) // set MAC
+			{
+		hndlr->set_mac((__u8 *) argv[2]);
+		// seeprom_set_mac(argv[2]);
+	} else if (strcmp(argv[1], "n") == 0) // set network's port types
+			{
+		hndlr->set_netw_port_map(argv[2]);
+		// seeprom_set_port_map(argv[2]);
+	} else if (strcmp(argv[1], "p") == 0) // set product name
+			{
+		hndlr->set_product_name(argv[2]);
+		// seeprom_set_product_name(argv[2]);
+	} else {
+		printf("Unknown parameter\n");
+		return cmd_usage(cmdtp);
+	}
+
+	return 0;
+}
+
+U_BOOT_CMD(sseepro, 7, 1, do_maintenance_sys_serial_eeprom, "Read/Maintenance System Serial EEPROM raw data",
+		"r - read raw data, f - display fields, w - primary format, e - erase, \n  \
+        b - set baseband serial, m - set MAC, n - set Ethernet port map, p - set Product Name");
+
