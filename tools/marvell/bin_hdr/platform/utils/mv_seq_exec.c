0dca23357e3c5b0458c7f24b29e45eb7834ae033
edwardk <edwardk@siklu.com>
2016-11-20 20:23:59 +0000

    add comment
    git-svn-id: http://192.168.42.160/software/main@18759 ce24552a-085e-4656-8849-d4bfd34db3c6

--- tools/marvell/bin_hdr/platform/utils/mv_seq_exec.c
+++ tools/marvell/bin_hdr/platform/utils/mv_seq_exec.c
@@ -59,6 +59,9 @@
    ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+   siklu_remarkM21   Setup serdes
+
 *******************************************************************************/
 
 #include "mv_seq_exec.h"
