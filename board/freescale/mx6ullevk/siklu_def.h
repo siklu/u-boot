7d8c98c83e884ee203cbc12d52031045d1cdf68a
edwardk <edwardk@siklu.com>
2017-12-26 15:02:16 +0000

    NXP: uboot remove unwanted declaration
    git-svn-id: http://192.168.42.160/software/main@22539 ce24552a-085e-4656-8849-d4bfd34db3c6

--- board/freescale/mx6ullevk/siklu_def.h
+++ board/freescale/mx6ullevk/siklu_def.h
@@ -9,9 +9,10 @@
 #define SIKLU_DEF_H_
 
 
-
+/* no need here, declared in include/configs/mx6ullevk_siklu.h
 #define MTDPARTS_DEFAULT   "mtdparts=gpmi-nand:128k(env_ro),128k(env_var0),128k(env_var1),128k(hdr0),40M(uimage0),"  \
     "128k(hdr1),40M(uimage1)," "16M(conf),-(log)"   // edikk TBD
+*/
 
 #define SIKLU_BIST_ENVIRONMENT_NAME "bist"
 #define SIKLU_BIST_MUT_ENVIRONMENT_NAME "SK_bist" // siklu_remark002
