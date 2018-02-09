ea73daba32537d55b137e2d67a84cc79be57a826
edwardk <edwardk@siklu.com>
2018-02-09 09:27:15 +0000

    NXP: uboot change partition name
    git-svn-id: http://192.168.42.160/software/main@22904 ce24552a-085e-4656-8849-d4bfd34db3c6

--- include/configs/mx6ullevk_siklu.h
+++ include/configs/mx6ullevk_siklu.h
@@ -34,7 +34,7 @@
 #define CONFIG_CMD_MTDPARTS	/* Enable MTD parts commands */
 #define CONFIG_MTD_DEVICE	/* needed for mtdparts commands */
 #define MTDIDS_DEFAULT		"nand0=gpmi-nand" //      SPI  ;spi1=spi_flash
-#define MTDPARTS_DEFAULT   "mtdparts=gpmi-nand:128k(env_ro),128k(env_var0),128k(env_var1),40M(uimage0),"  \
+#define MTDPARTS_DEFAULT   "mtdparts=gpmi-nand:128k(env),128k(env2),128k(env_t),40M(uimage0),"  \
     "40M(uimage1)," "16M(conf),-(log)"
 
 #define CONFIG_NAND_ECC_BCH
