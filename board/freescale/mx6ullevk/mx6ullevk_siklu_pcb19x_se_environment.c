92b72d8afd761881a12e378d866af8479a70a086
edwardk <edwardk@siklu.com>
2018-04-11 10:49:46 +0000

    NXP: uboot set default sys eeprom values
    git-svn-id: http://192.168.42.160/software/main@23495 ce24552a-085e-4656-8849-d4bfd34db3c6

--- board/freescale/mx6ullevk/mx6ullevk_siklu_pcb19x_se_environment.c
+++ board/freescale/mx6ullevk/mx6ullevk_siklu_pcb19x_se_environment.c
@@ -53,11 +53,11 @@ static sf_env_siklu_se_t* p_sf_env_siklu_se = &sf_env_siklu_se;
 static int syseeprom_access_init = 0;
 
 static const char siklu_default_environment_se[] = { //
-		"SE_product_name=EH8010" ";" //
+		"SE_product_name=8010FX" ";" //
 						"SE_mac=00:24:a4:00:de:ad" ";"//
 						"SE_board_serial=F123456789" ";"//
 						"SE_system_serial=0" ";"//
-						"SE_port_map=c---" ";" "\0" //
+						"SE_port_map=cd--" ";" "\0" //
 		};
 
 /*
