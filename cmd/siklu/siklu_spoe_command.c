3cf9521525b46c652924a4e35d2866761327b276
Vitaliy Sharapov <Vitaliy.Sharapov@udelphi.com>
2020-05-19 13:20:36 +0300

    Dev-1198. Added spoe command to u-boot

--- cmd/siklu/siklu_spoe_command.c
+++ cmd/siklu/siklu_spoe_command.c
@@ -1,43 +1,41 @@
 #include <common.h>
 #include <command.h>
-//#include <dm.h>
-//#include <spi.h>
-//#include <ubi_uboot.h>
-//#include <ubifs_uboot.h>
+#include <asm-generic/gpio.h>
 
 #include "common_boot.h"
-const int GPIO_POE_PIN = 39;
 
-static int do_siklu_poe_num_pairs_show(cmd_tbl_t * cmdtp, int flag, int argc,
-	char * const argv[]) {
-    int rc = CMD_RET_SUCCESS;
-/*
-    T_CPLD_LOGIC_DIP_MODE_REGS reg;
-    u8 poe_pair1_exist, poe_pair2_exist;
+static int do_siklu_poe_num_pairs_show(cmd_tbl_t * cmdtp, int flag, int argc, char * const argv[])
+{
+    unsigned gpio_no;
+    unsigned need_release = 0;
+    if(gpio_lookup_name("cpm_gpio18", NULL, NULL, &gpio_no))
+	goto Error;
 
-    reg.uint8 = siklu_cpld_read (CONFIG_CPLD_DIP_MODE_REG_ADDR);
+    if(gpio_request(gpio_no, "spoe"))
+	goto Error;
+    need_release = 1;
 
-    poe_pair1_exist = reg.s.cfg_poe_pair1_exist;
-    poe_pair2_exist = reg.s.cfg_poe_pair2_exist;
-*/
-    unsigned gpio_pin;
-    int r = gpio_lookup_name("cpm_gpio18", NULL, NULL, &gpio_pin);
-    printf("RES %d, PIN %d\n", r, gpio_pin);
-    //int r = gpio_direction_input(GPIO_POE_PIN);
-    //printf("Direction %d\n", r);
-    //int r2 = gpio_get_value(GPIO_POE_PIN);
-    //printf("Value %d\n", r2);
+    if(gpio_direction_input(gpio_no))
+	goto Error;
 
-/*
+    int pairs = gpio_get_value(gpio_no);
+    gpio_free(gpio_no);
 
-    if (poe_pair1_exist && poe_pair2_exist)
-	printf("No pairs\n");
-    else if (poe_pair1_exist || poe_pair2_exist)
+    if (pairs == 0)
 	printf("2-pairs\n");
-    else
+    else if(pairs == 1)
 	printf("4-pairs\n");
-*/
-    return rc;
+    else //Expected -1
+	goto Error;
+
+    return CMD_RET_SUCCESS;
+
+Error:
+    if(need_release)
+	gpio_free(gpio_no);
+    printf("Failed");
+    return CMD_RET_FAILURE;
 }
+
 U_BOOT_CMD(spoe, 5, 0, do_siklu_poe_num_pairs_show,
 	"Show POE number pairs Status", "Show POE number pairs Status");
