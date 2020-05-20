e25d97aae62282e8f7584496cdadef02d7c4a6c8
Vitaliy Sharapov <Vitaliy.Sharapov@udelphi.com>
2020-05-20 13:03:11 +0300

    Fixes accroding to review

--- cmd/siklu/siklu_spoe_command.c
+++ cmd/siklu/siklu_spoe_command.c
@@ -2,35 +2,38 @@
 #include <command.h>
 #include <asm/gpio.h>
 
-const char SPOE_NAME[] = "cpm_gpio18";
+const char N366_SPOE_NAME[] = "cpm_gpio18";
+#if 1
+#define SPOE_NAME N366_SPOE_NAME
+#else
+#error Need to be implemented
+#endif
 
 static int do_siklu_poe_num_pairs_show(cmd_tbl_t * cmdtp, int flag, int argc, char * const argv[])
 {
+	int ret = CMD_RET_FAILURE;
 	unsigned gpio_no;
-	unsigned need_release = 0;
 	const char * failure = NULL;
 	int pairs;
 	if(gpio_lookup_name(SPOE_NAME, NULL, NULL, &gpio_no))
 	{
 		failure = "gpio_lookup_name";
-		goto Error;
+		goto error;
 	}
 
 	if(gpio_request(gpio_no, "spoe"))
 	{
 		failure = "gpio_request";
-		goto Error;
+		goto error;
 	}
-	need_release = 1;
 
 	if(gpio_direction_input(gpio_no))
 	{
 		failure = "gpio_direction_input";
-		goto Error;
+		goto error_free_gpio;
 	}
 
 	pairs = gpio_get_value(gpio_no);
-	gpio_free(gpio_no);
 
 	if (pairs == 0)
 		printf("2-pairs\n");
@@ -39,16 +42,17 @@ static int do_siklu_poe_num_pairs_show(cmd_tbl_t * cmdtp, int flag, int argc, ch
 	else /*Expected -1 */
 	{
 		failure = "gpio_get_value";
-		goto Error;
+		goto error_free_gpio;
 	}
 
-	return CMD_RET_SUCCESS;
+	ret = CMD_RET_SUCCESS;
 
-Error:
-	if(need_release)
-		gpio_free(gpio_no);
-	printf("Failed on %s", failure);
-	return CMD_RET_FAILURE;
+error_free_gpio:
+	gpio_free(gpio_no);
+error:
+	if(ret != CMD_RET_SUCCESS)
+		printf("Failed on %s", failure);
+	return ret;
 }
 
 U_BOOT_CMD(spoe, 5, 0, do_siklu_poe_num_pairs_show,
