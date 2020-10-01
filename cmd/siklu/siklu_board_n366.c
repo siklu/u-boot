b7947f817efb85e89dd302b5f0e8644ca9162126
Shmuel H <shmuel.h@siklu.com>
2020-10-01 13:59:51 +0000

    Merged in fix/hw-id (pull request #21)
    cmd: n366: Fix HW id bits
    
    * cmd: n366: Fix HW id bits
    
    Approved-by: Dima Grinberg

--- cmd/siklu/siklu_board_n366.c
+++ cmd/siklu/siklu_board_n366.c
@@ -4,12 +4,11 @@
 #include <siklu/siklu_board_n366.h>
 #include <siklu/common_gpio.h>
 
-#define N366_NUM_OF_HW_REVISION_BITS 4
+#define N366_NUM_OF_HW_REVISION_BITS 3
 
 #define N366_HW_REVISION_GPIO_NAME_BIT_0 	"cpm_gpio126"  //  cpm bank 1 gpio 26 (CP0_MPP[58])
 #define N366_HW_REVISION_GPIO_NAME_BIT_1 	"cpm_gpio127"  //  cpm bank 1 gpio 27 (CP0_MPP[59])
-#define N366_HW_REVISION_GPIO_NAME_BIT_2 	"cps_gpio06"   //  cps bank 0 gpio 6  (CP1_MPP[6])
-#define N366_HW_REVISION_GPIO_NAME_BIT_3 	"cpm_gpio07"   //  cpm bank 0 gpio 7  (CP1_MPP[7])
+#define N366_HW_REVISION_GPIO_NAME_BIT_2 	"cps_gpio07"   //  cps bank 0 gpio 7  (CP1_MPP[7])
 
 
 /* Armada 7k/8k */
@@ -19,8 +18,7 @@
 static const char *hw_rev_gpio_bits_array[N366_NUM_OF_HW_REVISION_BITS]={
 	N366_HW_REVISION_GPIO_NAME_BIT_0, //LSB
 	N366_HW_REVISION_GPIO_NAME_BIT_1, 
-	N366_HW_REVISION_GPIO_NAME_BIT_2, 
-	N366_HW_REVISION_GPIO_NAME_BIT_3};
+	N366_HW_REVISION_GPIO_NAME_BIT_2};
 
 static int siklu_n366_get_hw_revision_by_gpio (u32 *hw_revision)
 {
